302ab8b0b115162b7c5e8a622e42d4a7f99e08d2
Steven Polglase <swpolgla@pm.me>
2026-02-17 18:47:16 -0500

    Initial Commit

---
+++ sparkcalc/sparkcalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkcalcApp.swift
+//  sparkcalc
+//
+//  Created by Steven Polglase on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct sparkcalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
